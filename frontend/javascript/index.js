9871997eeadfa8ec598519c2830718b0bdf0c27a
Lars Peters <lp@itpeters.com>
2023-08-28 16:47:09 +0200

    Local JS/CSS

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,5 +1,8 @@
 import "index.css"
 import "fontawesome.js"
+import "popper.js"
+import "jquery.js"
+import "bootstrap.min.js"
 
 // Import all JavaScript & CSS files from src/_components
 import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
