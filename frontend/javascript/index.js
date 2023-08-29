7a493319d675209a476b5ccd5f9539a458c0044c
Lars Peters <lp@itpeters.com>
2023-08-29 11:22:56 +0200

    Include commentbox.io in JS build

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -8,5 +8,11 @@ import "solid.min.js"
 import "brands.min.js"
 import "fontawesome.min.js"
 
+// CommentBox.io
+import commentBox from "commentbox.io";
+commentBox('5766294217424896-proj', {
+  sortOrder: 'newest'
+});
+
 // Import all JavaScript & CSS files from src/_components
 import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
