9ffc86f02d60fcb0d82951f5a15c2fb51bac6f14
Lars Peters <lp@itpeters.com>
2023-08-29 09:31:02 +0200

    Fonts and icons locally embedded

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,8 +1,12 @@
 import "index.css"
-import "fontawesome.js"
 import "popper.js"
 import "jquery.js"
 import "bootstrap.min.js"
+
+// Fontawesome
+import "solid.min.js"
+import "brands.min.js"
+import "fontawesome.min.js"
 
 // Import all JavaScript & CSS files from src/_components
 import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
